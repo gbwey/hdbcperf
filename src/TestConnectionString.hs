9d1ca3665cca43bc9d990327dcbf6ff511edd3ce
grantwey <grantwey@med.umich.edu>
2018-07-17 12:45:05 -0400

    initial commit

--- src/TestConnectionString.hs
+++ src/TestConnectionString.hs
@@ -1,5 +1,4 @@
 module TestConnectionString where
 
 connstr :: String  
-connstr = "Driver={SQL Server Native Client 11.0};Server=NTSRODBSDV1.umhs.med.umich.edu;Database=AltRoar;uid=grantwey;pwd=grantwey;MARS_Connection=Yes;"  
---connstr = "Driver={SQL Server Native Client 11.0};Server=NTSRODBSDV1.umhs.med.umich.edu;Database=AltRoar;uid=grantwey;pwd=grantwey;"  
+connstr = "Driver={SQL Server Native Client 11.0};Server=?;Database=?;uid=?;pwd=?;MARS_Connection=Yes;"  
