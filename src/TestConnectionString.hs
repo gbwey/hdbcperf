720ea41b44b78933bcd35e95305a17d8609fb639
grantwey <grantwey@med.umich.edu>
2018-07-25 17:12:37 -0400

    minor

--- src/TestConnectionString.hs
+++ src/TestConnectionString.hs
@@ -1,4 +1,4 @@
 module TestConnectionString where
 
 connstr :: String  
-connstr = "Driver={SQL Server Native Client 11.0};Server=?;Database=?;uid=?;pwd=?;MARS_Connection=Yes;"  
+connstr = "Driver={SQL Server Native Client 11.0};Server=?;Database=?;uid=?;pwd=?;MARS_Connection=No;"  
